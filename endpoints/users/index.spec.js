a82c9d34344952e39f0fe3fdb236835e9dd8def8
Rommel Ayala <rommel.ayala@veepee.com>
2022-03-23 19:37:39 +0100

    todos los test y code ok

--- endpoints/users/index.spec.js
+++ endpoints/users/index.spec.js
@@ -37,5 +37,97 @@ describe('Endpoints', () => {
 
             })
         })
+        describe('post',()=>{
+            it('create to user json', async () => {
+                const axios = {
+                    post: jest.fn().mockResolvedValue({ data: 1 })
+                }
+
+                const res =  {
+                    status: jest.fn().mockReturnThis(),
+                    send: jest.fn()
+                }
+                const req = {
+                    body: 'requeste body'
+                }
+
+                await handlers({ axios }).post(req, res)
+
+                //console.log(res.status.mock)
+                expect(res.status.mock.calls).toEqual([
+                    [201]
+                ])
+                
+                expect(res.send.mock.calls).toEqual([
+                    [1]
+                ])
+                
+                expect(req.body).toEqual('requeste body')
+                
+                //console.log(axios.post.mock.calls)              
+                expect(axios.post.mock.calls).toEqual(
+                    [ [ 'https://jsonplaceholder.typicode.com/users', 'requeste body' ] ]
+                )
+                
+                //console.log(req.body)
+                expect(req.body).toEqual('requeste body')
+
+            })
+        })
+        describe('put',()=>{
+            it('update to user json', async () => {
+
+                const axios = {
+                    put: jest.fn().mockResolvedValue({ data: 1 })
+                }
+
+                const req = {
+                    body: 'requeste body',
+                    params: {
+                        id: 12
+                    }
+                }
+                const res =  {
+                    sendStatus: jest.fn()
+                }
+                
+                await handlers({ axios }).put(req, res)
+
+                //console.log(axios.put.mock.calls)
+                //console.log(req.params.id)              
+                expect(axios.put.mock.calls).toEqual(
+                    [ [ 'https://jsonplaceholder.typicode.com/users/12', 'requeste body' ] ]
+                )
+                //console.log(res.sendStatus.mock)
+                expect(res.sendStatus.mock.calls).toEqual([
+                    [204]
+                ])
+                
+            })
+        })
+        describe('delete',()=>{
+            it('delete one user',async ()=>{
+                const axios = {
+                    delete: jest.fn().mockResolvedValue({ data: 1 })
+                }
+                const req = {
+                    params: {
+                        id: 12
+                    }
+                }
+                const res =  {
+                    sendStatus: jest.fn()
+                }
+                await handlers({ axios }).delete(req, res)
+                //console.log(res.sendStatus.mock.calls)
+                expect(res.sendStatus.mock.calls).toEqual([
+                    [204]
+                ])
+                //console.log(axios.delete.mock.calls)
+                expect(axios.delete.mock.calls).toEqual([
+                    [ 'https://jsonplaceholder.typicode.com/users/12' ]
+                ])
+            })
+        })
     })
 })
